423ee3d14f79fde00f9353bfe9c688444a71c946
Akshatchourey <achourey09@gmail.com>
2025-03-27 22:06:57 +0530

    started working on deployment, also worked on environment variables, storages, media files, s3 etc.

--- PahalFoundation/content/models.py
+++ PahalFoundation/content/models.py
@@ -16,3 +16,32 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+class Video(models.Model):
+    sno = models.AutoField(primary_key=True)
+    pf = models.CharField(max_length=1)
+    title = models.CharField(max_length=200)
+    visible = models.BooleanField()  # visibility 1-visible
+    desc = models.TextField()
+    thumbnail = models.ImageField(upload_to ='video_thumbnail/')
+    date = models.DateTimeField(auto_now_add=True)
+    views = models.PositiveIntegerField()
+    likes = models.PositiveIntegerField()
+    slug = models.CharField(max_length=25)
+    source = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.slug
+
+
+class Playlist(models.Model):
+    sno = models.AutoField(primary_key=True)
+    pf = models.CharField(max_length=1)
+    title = models.CharField(max_length=200)
+    visible = models.BooleanField()  # visibility 1-visible
+    desc = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    thumbnail = models.ImageField(upload_to ='playlist_thumbnail/')
+    slug = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.slug
--- PahalFoundation/content/views_teacher.py
+++ PahalFoundation/content/views_teacher.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Blog
 
 @login_required(login_url='/login/')
 def create_blog(request):
+    if request.method == "POSt":
+        title = request.POST.get('blog_title')
+        content = request.POST.get('main_content')
+
+        new_blog = Blog()
+        new_blog.save()
+        return redirect(request, "/your_blogs/")
+
     return render(request, 'content/blogcreate.html')
